bfb852a50663024ad3bf2395a649d82bf3847b24
josh.brown <josh.brown@COMP10583.local>
2019-09-09 12:00:45 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t jeb2162/multi-client:latest -t jeb2162/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jeb2162/multi-worker:latest -t jeb2162/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jeb2162/multi-server:latest -t jeb2162/multi-server:$SHA -f ./server/Dockerfile ./server
+docker push jeb2162/multi-client:latest
+docker push jeb2162/multi-client:$SHA
+docker push jeb2162/multi-server:latest
+docker push jeb2162/multi-server:$SHA
+docker push jeb2162/multi-worker:latest
+docker push jeb2162/multi-worker:$SHA
+kubectl appply -f k8s
+kubectl set image deployments/server-deployment server=jeb2162/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jeb2162/multi-client:$SHA
+kubectl set image deployments/worker-deployment client=jeb2162/multi-worker:$SHA
+
+
